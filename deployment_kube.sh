512179440c565b67227184a62d5f70277e0e8dff
qualite <117730700+mathiscapart@users.noreply.github.com>
2025-02-14 00:47:41 +0100

    update

--- deployment_kube.sh
+++ deployment_kube.sh
@@ -1,7 +1,5 @@
-apt install -y vim open-iscsi sudo curl nfs-common cryptsetup dmsetup jq # run to different server pods
+apt install -y vim open-iscsi sudo curl nfs-common cryptsetup dmsetup jq
 curl -sfL https://get.k3s.io | sh -s - --disable=traefik --disable=servicelb
-cat  /var/lib/rancher/k3s/server/node-token
-cat /etc/rancher/k3s/k3s.yaml #change .kube/config
 
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.12.0/deploy/static/provider/cloud/deploy.yaml
 
@@ -16,4 +14,3 @@ kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.7.2/depl
 kubens longhorn-system
 kubectl apply -f longhorn/longhorn-ingress.yml
 kubectl apply -f longhorn/secret-longhorn.yml
-
